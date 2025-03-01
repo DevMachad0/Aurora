5b29591b54e80d4ccf9ba104a8b206669240f0ec
DEIVID KEVIN DA SILVA MACHADO <135076300+DevMachad0@users.noreply.github.com>
2025-02-28 23:22:12 -0300

    Update login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
 
-  const res = await fetch('/login', {
+  const res = await fetch('https://aurora-7j74.onrender.com/api/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
