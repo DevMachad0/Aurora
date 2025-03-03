bb63bfe53dc728c6381e1125dddcc997c020d7ea
Devmachado <deividkevin735@gmail.com>
2025-03-03 16:26:56 -0300

    desfazendo

--- index.js
+++ index.js
@@ -7,53 +7,16 @@ const userRoutes = require("./src/routes/userRoutes");
 const chatRoutes = require("./src/routes/chatRoutes");
 const chatHistoryRoutes = require("./src/routes/chatHistoryRoutes");
 const storageRoutes = require("./src/routes/storageRoutes");
-const chatSupportRoutes = require('./src/routes/chat_support');
-const AuthenticatedDomain = require('./src/models/authenticatedDomainsModel');
+const chatSupportRoutes = require('./src/routes/chat_support'); 
 
 const app = express();
 app.use(express.json());
-
-// Função para carregar domínios autenticados
-async function getAuthenticatedDomains() {
-    try {
-        const domains = await AuthenticatedDomain.find().distinct('dominios');
-        return domains;
-    } catch (error) {
-        console.error('Erro ao buscar domínios autenticados:', error);
-        return [];
-    }
-}
-
-// Configuração de CORS (agora com tratamento explícito para requisições OPTIONS)
-async function configureCors() {
-    const allowedOrigins = await getAuthenticatedDomains(); // Pega os domínios autenticados
-    
-    app.use((req, res, next) => {
-        const origin = req.headers.origin;
-
-        if (!origin || allowedOrigins.includes(origin)) {
-            // Permite a origem se ela estiver na lista de domínios autenticados
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        } else {
-            // Bloqueia a origem se não estiver na lista
-            res.setHeader('Access-Control-Allow-Origin', ''); // Bloqueia a origem
-        }
-
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Métodos permitidos
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
-        res.setHeader('Access-Control-Allow-Credentials', 'true'); // Permite cookies
-
-        // Responde às requisições OPTIONS (preflight request)
-        if (req.method === 'OPTIONS') {
-            return res.status(200).end();
-        }
-
-        next(); // Continua para o próximo middleware
-    });
-}
-
-// Executa a configuração do CORS após carregar os domínios
-configureCors();
+// Configuração do CORS (deve ser feita antes de definir as rotas)
+app.use(cors({
+    origin: 'https://aurorati.tech', // Permite todas as origens
+    methods: ['GET', 'POST'], // Métodos permitidos
+    allowedHeaders: ['Content-Type'] // Cabeçalhos permitidos
+}));
 
 // Servindo arquivos estáticos da pasta 'public'
 app.use(express.static("public"));
@@ -75,7 +38,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html"); // Envia o arquivo index.html
 });
 
-// Definindo as rotas
 app.use("/users", userRoutes);
 app.use('/api', authRoutes); // Definir rota para login
 app.use('/api', chatSupportRoutes);
