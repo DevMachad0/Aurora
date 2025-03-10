00860da0058291f04162d15ae96034c97d39ac21
Devmachado <deividkevin735@gmail.com>
2025-03-10 16:02:19 -0300

    a

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("../models/userModel");
 require("dotenv").config();
 
 passport.use(new GoogleStrategy({
@@ -10,17 +9,7 @@ passport.use(new GoogleStrategy({
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = new User({
-          googleId: profile.id,
-          nome: profile.displayName,
-          email: profile.emails[0].value,
-          // Adicione outros campos necessários aqui
-        });
-        await user.save();
-      }
-      return done(null, user);
+      return done(null, profile);
     } catch (error) {
       return done(error, null);
     }
@@ -33,8 +22,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findById(id);
-    done(null, user);
+    // ...existing code...
   } catch (error) {
     done(error, null);
   }
