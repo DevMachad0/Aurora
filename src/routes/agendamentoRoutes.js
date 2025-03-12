98db0574815516d2a76bf1cb6bde1543d01bd56f
Devmachado <deividkevin735@gmail.com>
2025-03-12 09:26:36 -0300

    A

--- src/routes/agendamentoRoutes.js
+++ src/routes/agendamentoRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get("/agendamentos", async (req, res) => {
     try {
         const email = req.headers["user-email"];
-        const empresa = req.headers["user-empresa"];
-        const database = req.headers["user-database"];
+        const empresa = req.headers["user-empresa"]?.trim();
+        const database = req.headers["user-database"]?.trim();
 
         if (!email || !empresa || !database) {
             return res.status(400).json({ error: "Email, empresa ou database do usuário não encontrado" });
@@ -50,15 +50,18 @@ router.post("/agendamentos/excluir", async (req, res) => {
     try {
         const { titulo, data, hora } = req.body;
         const email = req.headers["user-email"];
-        const empresa = req.headers["user-empresa"];
-        const database = req.headers["user-database"];
+        const empresa = req.headers["user-empresa"]?.trim();
+        const database = req.headers["user-database"]?.trim();
+
         if (!email || !empresa || !database) {
             return res.status(400).json({ error: "Email, empresa ou database do usuário não encontrado" });
         }
 
         const db = mongoose.connection.useDb(database);
-        const collectionName = `data_${empresa}`; // Usar o nome da empresa com espaços
-        console.log(`Nome da coleção formatado: ${collectionName}`); // Log para depuração
+        const collectionName = database.startsWith("data_") ? database : `data_${empresa}`;
+        
+        console.log(`Nome da coleção formatado corretamente: ${collectionName}`); // Log para depuração
+
         const collection = db.collection(collectionName);
 
         const result = await collection.updateOne(
